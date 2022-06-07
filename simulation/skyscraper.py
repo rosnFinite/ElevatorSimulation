dadb354f0f1fd47380c95a57dbc21beaf61207da
Fabio Rose <rosef@hochschule-trier.de>
2022-06-07 13:50:39 +0200

    removed call of deleted plot_data

--- simulation/skyscraper.py
+++ simulation/skyscraper.py
@@ -201,5 +201,4 @@ if __name__ == "__main__":
     sky = Skyscraper(random_seed=12345)
     # time = 8640  // 1 sim step = 10 sec
     sky.run_simulation(config.SIMULATION_TIME)
-    sky.plot_data()
     print(sky.statistics())
